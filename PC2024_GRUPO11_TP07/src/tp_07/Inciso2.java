9598b3c1cf96f322348ba9ac8520fb3c49180429
danie <danie@AjallaDaniel>
2024-10-01 23:42:37 -0300

    correcion

--- PC2024_GRUPO11_TP07/src/tp_07/Inciso2.java
+++ PC2024_GRUPO11_TP07/src/tp_07/Inciso2.java
@@ -3,16 +3,13 @@ package tp_07;
 import java.util.concurrent.Semaphore;
 
 public class Inciso2 {
-	private static int A = 50;
-    private static int B = 150;
-    private static Semaphore semaforo = new Semaphore(1,true);
+	private static int X = 0;
     private static Semaphore semaforo1 = new Semaphore(1,true);
+    private static Semaphore semaforo2 = new Semaphore(0,true);
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(new Tarea1());
-        Thread t2 = new Thread(new Tarea2());
-        t1.start();
-        t2.start();
-        System.out.println("Resultado final: A = " + A + ", B = " + B);
+        new Thread(new Tarea1()).start();
+        new Thread(new Tarea2()).start();
+        new Thread(new Tarea3()).start();
     }
 
     private static class Tarea1 implements Runnable {
@@ -20,23 +17,15 @@ public class Inciso2 {
         public void run() {
         	while(true) {
         		 try {
-                     semaforo.acquire();
-                     A = A+100;
-                     System.out.println("Tarea 1: variable1 = " +A);
-                 } catch (InterruptedException e) {
-                     Thread.currentThread().interrupt();
-                 } finally {
-                    semaforo.release();
-                }
-            	 try {
+                     semaforo2.acquire();
                      semaforo1.acquire();
-                     B = B-100;
-                     System.out.println("Tarea 1: variable2 = " +B);
+                     X = 2*X;
+                     System.out.println("Tarea 1: valor X = " +X);
                  } catch (InterruptedException e) {
                      Thread.currentThread().interrupt();
                  } finally {
                     semaforo1.release();
-                } 
+                }
         	}
         }
     }
@@ -46,24 +35,32 @@ public class Inciso2 {
         public void run() {
         	 while(true) {
         		 try {
-                     semaforo.acquire();
-                    A =(int) 1.21* A;
-                    System.out.println("Tarea 2: variable1 = " +A);
+                     semaforo1.acquire();
+                     X = X*X;
+                    System.out.println("Tarea 2: valor X = " +X);
                  } catch (InterruptedException e) {
                      Thread.currentThread().interrupt();
                  }finally {
-                    semaforo.release();
-                }
-            	 try {
-                     semaforo1.acquire();
-                     B =(int) 0.5 * B;
-                     System.out.println("Tarea 2: variable2 = " +B);
-                 } catch (InterruptedException e) {
-                     Thread.currentThread().interrupt();
-                 } finally {
                     semaforo1.release();
                 }
         	 }
+        }
+    }
+    private static class Tarea3 implements Runnable {
+        @Override
+        public void run() {
+        	while(true) {
+        		 try {
+                     semaforo1.acquire();
+                     X = X + 3;
+                     System.out.println("Tarea 3: valor X = " +X);
+                 } catch (InterruptedException e) {
+                     Thread.currentThread().interrupt();
+                 } finally {
+                    semaforo2.release();
+                    semaforo1.release();
+                }
+        	}
         }
     }
 }
